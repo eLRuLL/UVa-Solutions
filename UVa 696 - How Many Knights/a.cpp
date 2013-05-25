2dd4deb8f229e47c4cd3c04fdf60d8efb08e46f4
Raul Gallegos <raul.ogh@gmail.com>
2013-05-25 00:34:41 -0500

    Uva 696: care for case with 1 and 2

---
+++ UVa 696 - How Many Knights/a.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define max(x,y)	(x>y)?x:y
+#define min(x,y)	(x<y)?x:y
+
+int main(){
+	int m,n,res;
+	scanf("%d %d",&m,&n);
+	while(m!=0 || n!=0){
+		if(m==0 || n==0){
+			res = 0;
+		}else if(m==1 || n==1){
+			res = max(m,n);
+		}else if(m==2 || n==2){
+			res = (min((max(m,n)%4),2))*2 + (max(m,n)/4)*4;
+		}else{
+			res = (m*n+1)>>1;
+		}
+		printf("%d knights may be placed on a %d row %d column board.\n",res,m,n);
+		scanf("%d %d",&m,&n);
+	}
+	return 0;
+}
